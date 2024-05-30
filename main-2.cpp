4d205f9e9dc9d3ba8519d3e55b57b12e223864e7
TJgreentea <161820260+TJgreentea@users.noreply.github.com>
2024-05-30 13:48:59 +0930

    Add files via upload

---
+++ main-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Snare.h"
+#include"Persona.h"
+int main(){
+   Persona(10,10);
+   std::cout<<"intial location: "<<std::get<0>persona.getLoc()<<std::get<1> persona>getLoc<<
+   ship shift(2,3);
+   std::cout<<"afer move"<<std::get<0>persona.getLoc()<<std::get<1> persona>getLoc<<
+   ship shift(2,3);
+   
+}
